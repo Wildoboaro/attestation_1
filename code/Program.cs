98452d1b301d133c794e0909d9ebbc64ed6ced01
P.Bor <p.bor@treecolor.spb.ru>
2024-01-07 19:58:17 +0200

    add code

---
+++ code/Program.cs
@@ -0,0 +1,22 @@
+﻿string [] ShotStringFinder(string [] inputArray)
+{
+    string [] outputArray= new string[inputArray.Length]; 
+    int resCount = 0;
+    foreach(string elemStr in inputArray)
+    {
+        if (elemStr.Length <= 3)
+        {
+            outputArray[resCount] = elemStr;
+            resCount++;
+        }
+    }
+    Array.Resize(ref outputArray, resCount);
+    return outputArray;
+}
+
+Console.Clear();
+// string[] stringArray = {"Russia", "Denmark", "Kazan"};
+ string[] stringArray = {"1234", "1567", "-2", "computer science"};
+// string[] stringArray = {"Hello", "2", "world", ":-)"};
+string[] stringResult = ShotStringFinder(stringArray);
+Console.WriteLine(String.Join(", ", stringResult));
